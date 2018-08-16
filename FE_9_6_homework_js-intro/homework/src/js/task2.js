5f85b1bd9999d62ee73e47fd3718d071ca22b03c
Adriana Kusiak <31694326+Tisifona@users.noreply.github.com>
2018-08-16 23:44:59 +0300

    Update task2.js

--- FE_9_6_homework_js-intro/homework/src/js/task2.js
+++ FE_9_6_homework_js-intro/homework/src/js/task2.js
@@ -4,6 +4,7 @@ const a_length = parseFloat(prompt(`Please enter length of first side`));
 const b_length = parseFloat(prompt(`Please enter of second side`));
 const angle = parseFloat(prompt(`Please enter angle`));
 const all_angles = 180;
+let output;
 
 const c_length = Math.sqrt(a_length*a_length + b_length*b_length - 2*a_length*b_length*
     Math.cos(Math.PI/all_angles*angle));
@@ -11,9 +12,10 @@ const square = a_length * b_length * Math.sin(Math.PI/all_angles*angle)/2;
 const perimeter = a_length + b_length + c_length;
 
 if(a_length > 0 && b_length > 0 && angle > 0){
-    console.log(`c length: ` + parseFloat(c_length.toFixed(2)) + '\n'
+    output = `c length: ` + parseFloat(c_length.toFixed(2)) + '\n'
     + `Triangle square: ` + parseFloat(square.toFixed(2)) + '\n' + `Triangle perimeter: ` +
-    parseFloat(perimeter.toFixed(2)));
+    parseFloat(perimeter.toFixed(2));
 } else{
-    console.log(`Invalid data`);
+    output = `Invalid data`;
 }
+    console.log(output);
