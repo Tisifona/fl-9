ee3e962d61f89c5b9513af28061942e191828480
Adriana Kusiak <31694326+Tisifona@users.noreply.github.com>
2018-08-16 23:43:31 +0300

    Update task1.js

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -7,9 +7,10 @@ const saved = amount_of_money * discount/100;
 const price_with_discount = amount_of_money - saved;
 
 if (amount_of_money > 0 && discount >= 0 && discount <= 100) {
-    console.log(`Price without discount: ` + parseFloat(amount_of_money.toFixed(2)) + '\n' + `Discount: `
+    output = `Price without discount: ` + parseFloat(amount_of_money.toFixed(2)) + '\n' + `Discount: `
         + parseFloat(discount.toFixed(2)) + `%` + '\n' + `Price with discount: ` +
-        parseFloat(price_with_discount.toFixed(2)) + '\n' + `Saved: ` + parseFloat(saved.toFixed(2)))
+        parseFloat(price_with_discount.toFixed(2)) + '\n' + `Saved: ` + parseFloat(saved.toFixed(2));
 } else {
-    console.log(`Invalid data`)
+    output = `Invalid data`;
 }
+    console.log(output);
