a90669670b86191cf72fbb898271d4ba6872efd3
Tisifona <adriana.kusiak.kn.2016@lpnu.ua>
2018-08-21 19:03:27 +0300

    Finished homework 7

---
+++ FE_9_7_homework_control-flow/homework/src/js/task1.js
@@ -0,0 +1,32 @@
+// Your code goes here
+let numberFour = 4;
+let numberTwenty = 20;
+
+const login = prompt(`Please enter your login`);
+
+
+if (login === `` || login === null) {
+    alert(`Canceled.`);
+} else if(login.length < numberFour) {
+    alert(`I don't know any users having name length less than 4 symbols`);
+} else if (login !== `User`) {
+    alert(`I don’t know you`);
+} else {
+    const password = prompt(`Please enter your password`);
+
+    if (password === `` || password === null){
+        alert(`Canceled`);
+    } else{
+        if (password !== `SuperUser` ) {
+            alert(`Wrong password`);
+        } else {
+            if (new Date().getHours() < numberTwenty) {
+                alert(`Good day!`);
+            } else {
+                alert(`Good evening!`);
+            }
+        }
+    }
+
+    
+}
---
+++ FE_9_7_homework_control-flow/homework/src/js/task2.js
@@ -0,0 +1,59 @@
+// Your code goes here
+let minNumber = 0;
+let maxNumber = 5;
+let maxPrize = 10;
+let magicNumberOne = 1;
+let magicNumberTwo = 2;
+let magicNumberThree = 3;
+let magicNumberFour = 4;
+
+function getRandom(min, max) {
+    let one = 1;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + one)) + min;
+}
+
+function calcPrize(numberOfPlay, i) {
+    return Math.floor(Math.pow(magicNumberThree, numberOfPlay - magicNumberOne) * maxPrize /
+        Math.floor(Math.pow(magicNumberTwo, i - magicNumberOne)));
+}
+
+function startGame(numberOfPlay, money) {
+    let random_number = getRandom(minNumber, maxNumber);
+    let number;
+    for (let i = 1; i < magicNumberFour; i++) {
+        number = parseFloat(prompt(`Enter a number from ` + minNumber + ` to ` + maxNumber + `\n` + `Attempts left: `
+            + i + `\n` + `Total prize: ` + money + `$` + `\n` + `Possible prize on current attempt: ` +
+            Math.floor(Math.pow(magicNumberThree, numberOfPlay - magicNumberOne) * maxPrize /
+                Math.floor(Math.pow(magicNumberTwo, i - magicNumberOne))) + `$`));
+        if (random_number === number) {
+            money += calcPrize(numberOfPlay, i);
+            if (confirm(`Congratulation! Your prize is:  ` + money + `\n` + `Do you want to continue?`)) {
+                maxNumber *= magicNumberTwo;
+                startGame(++numberOfPlay, money);
+            } else {
+                break;
+            }
+        } else {
+            alert(`Please try again`);
+        }
+    }
+
+    if (confirm(`Do you want to play again?`)) {
+        startGame(magicNumberOne, 0)
+    } else {
+        alert(`You did not become a millionaire, but can.`);
+    }
+}
+
+
+const playGame = confirm(`Do you want to play a game?`);
+
+
+if (playGame === false) {
+    alert(`You did not become a millionaire, but can.`);
+} else {
+    startGame(magicNumberOne, 0);
+
+}
