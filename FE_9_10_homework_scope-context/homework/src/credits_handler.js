8d355fdaa930607c8128462839207b7d794c5533
Tisifona <adriana.kusiak.kn.2016@lpnu.ua>
2018-09-02 20:01:50 +0300

    Finished homework 10

--- FE_9_10_homework_scope-context/homework/src/credits_handler.js
+++ FE_9_10_homework_scope-context/homework/src/credits_handler.js
@@ -1,47 +1,53 @@
-/* Your code goes here */
-
 function userCard(key) {
     let balance = 100;
     let transactionLimit = 100;
-    let historyLogs = 100;
+    let historyLogs = [];
 
 
     return {
         getCardOptions() {
             return {balance, transactionLimit, historyLogs, key};
         },
-        putCredits(amountCredits){
-            balance+=amountCredits;
+        putCredits(amountCredits) {
+            balance += amountCredits;
 
             historyLogs.push({
-               operationType: 'Received credits',
+                operationType: 'Received credits',
                 credits: amountCredits,
-                //дата кредиту
+                operationDate: new Date().toLocaleString('en-GB')
             });
         },
-        takeCredits(amountCredits){
-            if(transactionLimit > amountCredits && balance > amountCredits){
-                balance-=amountCredits;
+        takeCredits(amountCredits) {
+            if (transactionLimit > amountCredits && balance > amountCredits) {
+                balance -= amountCredits;
 
                 historyLogs.push({
                     operationType: 'Withdrawal credits',
                     credits: amountCredits,
-                    //дата кредиту
+                    operationDate: new Date().toLocaleString('en-GB')
                 });
-            }else {
+            } else {
                 console.log('Sorry, you can not take credits from the card!');
             }
         },
-        setTransactionLimit(amountCredits){
+        setTransactionLimit(amountCredits) {
             transactionLimit = amountCredits;
             historyLogs.push({
                 operationType: 'Transaction limit change',
                 credits: amountCredits,
-                //дата кредиту
+                operationDate: new Date().toLocaleString('en-GB')
             });
+        },
+        transferCredits(amountCredits, card) {
+            const tax = 0.005;
+            if (transactionLimit > amountCredits && balance > amountCredits) {
+                amountCredits -= amountCredits * tax;
+                card.putCredits(amountCredits);
+            } else {
+                console.log('Sorry, you can not perform this operation!');
+            }
         }
     }
-
 }
 
 class UserAccount {
@@ -49,5 +55,21 @@ class UserAccount {
     constructor(name) {
         this.name = name;
         this.cards = [];
+        this.limit = 3;
+    }
+
+    addCard() {
+        if (this.cards.length >= this.limit) {
+            console.log('Sorry, you can not perform this operation!');
+        } else {
+            let card = userCard(this.cards.length + 1);
+            this.cards.push(card);
+        }
+    }
+
+    getCardByKey(key) {
+        return this.cards[key - 1];
+
     }
 }
+
